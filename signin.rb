46cc899628611d02ce8e7dcf21fa9c06fdb3a45d
Douglas Berkley <douglasmberkley@gmail.com>
2025-07-21 15:49:16 +0900

    debugged with better output

--- signin.rb
+++ signin.rb
@@ -1,6 +1,10 @@
 require "io/console"
 require "json"
 
+def clone_repo(batch_number)
+  system("git clone git@github.com:dmbf29/food-delivery-#{batch_number}.git")
+end
+
 def signin_choice(batch_number)
   system("cp -rf food-delivery-#{batch_number}/app/views/sessions_view.rb app/views")
   system("cp -rf food-delivery-#{batch_number}/app/controllers/sessions_controller.rb app/controllers")
@@ -35,7 +39,8 @@ directory_location = `pwd`.strip
 correct_dir = "05-Food-Delivery-Day-Two/01-Food-Delivery"
 
 if directory_location[-41..-1] == correct_dir
-  puts "You're in the right location 👌"
+  puts "You're in the right directory 👌, continuing..."
+  puts
 else
   puts "⚠️ You're in the wrong place. You need to be in: #{correct_dir}"
   return
@@ -44,28 +49,26 @@ end
 puts "What's your batch number?"
 batch_number = gets.chomp
 
-puts "Cloning live code repo..."
-succeed = system("git clone git@github.com:dmbf29/food-delivery-#{batch_number}.git")
-unless succeed
-  puts "⚠️⚠️⚠️"
-  puts "Looks like something went wrong. Did you enter the correct number? Is there a repository at: 'www.github.com/dmbf29/food-delivery-#{batch_number}'"
-  puts "⚠️⚠️⚠️"
-  return
-end
-
 puts "Choose ONE option:"
 puts "replace -> I just want to replace everything from yesterday with our livecode"
 puts "signin -> My code was working yesterday, I just want the sign in logic"
 choice = nil
-until ['signin', 'replace'].includes?(choice)
+until ['replace', 'signin'].include?(choice)
   puts
   puts "replace or signin"
   choice = gets.chomp.downcase
 end
 
-case choice
-when 'signin' then signin_choice(batch_number)
-when 'replace' then replace_choice(batch_number)
+succeed = clone_repo(batch_number)
+if succeed
+  case choice
+  when 'signin' then signin_choice(batch_number)
+  when 'replace' then replace_choice(batch_number)
+  end
+else
+  puts "⚠️⚠️⚠️"
+  puts "Looks like something went wrong. Did you enter the correct number? Is there a repository at: 'www.github.com/dmbf29/food-delivery-#{batch_number}'"
+  puts "⚠️⚠️⚠️"
 end
 
 
